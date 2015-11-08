7d892d7f935a95df8ffab0010df35838f96cf93d
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-11-08 18:21:32 -0500

    Removed nonfunctioning authentication code

--- firstapp/app/controllers/Application.java
+++ firstapp/app/controllers/Application.java
@@ -7,20 +7,26 @@ import views.html.*;
 
 public class Application extends Controller {
 
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+    }
+
     public Result index() {
         return ok(index.render("Zentasks will be here   ."));
     }
+    // 
+    // public Result login() {
+    //     return ok(
+    //         login.render(form(Login.class))
+    //     );
+    // }
+    // public Result authenticate() {
+    //     login.render(form(Login.class));
+    //     return ok();
+    // }
 
-    public Result login() {
-        return ok(
-            login.render(form(Login.class))
-        );
-    }
-
-public class Login {
-
-    public String email;
-    public String password;
-
-}
 }
