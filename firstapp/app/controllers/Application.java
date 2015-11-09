0a44b9570dd17189444952e35e875cfb0f5dc98b
joshfram <jpf2141@columbia.edu>
2015-11-08 22:30:47 -0500

    CSS Style sheet for cover page works

--- firstapp/app/controllers/Application.java
+++ firstapp/app/controllers/Application.java
@@ -12,22 +12,14 @@ public class Application extends Controller {
          return ok(index.render());
      }
 
-<<<<<<< HEAD
+
     public Result login() {
         return ok(
-            login.render("Hello")
+            login.render()
         );
     }
 
-=======
->>>>>>> dd6c269eb8aa6d645183050dbbcb8eb820dd256a
 
 
 
 }
-<<<<<<< HEAD
-
-}
-
-=======
->>>>>>> dd6c269eb8aa6d645183050dbbcb8eb820dd256a
